cffaafb1458b393bf3613de16c57fbe803a67d3e
Sala_06 <Sala_06@CUS06E25>
2022-11-10 19:48:42 -0500

    Termine

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -15,6 +15,29 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
     }
     
 }
